2b5d35c8c0fc6158ec3f6432c95c2d9d184066b5
premkumar1809 <143527746+premkumar1809@users.noreply.github.com>
2024-03-01 12:15:18 +0000

    Update Firebase.js

--- frontend/src/Firebase.js
+++ frontend/src/Firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getStorage} from 'firebase/storage';
 import {getFirestore} from "firebase/firestore"
 
-// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB9NomQPgAoQ6B1c64XhN3ahqN0U1Fe3YU",
-  authDomain: "mern-df125.firebaseapp.com",
-  projectId: "mern-df125",
-  storageBucket: "mern-df125.appspot.com",
-  messagingSenderId: "757150184874",
-  appId: "1:757150184874:web:16b30a921277ea328527d3"
+  apiKey: "AIzaSyD6wdlkdN3iR8oxDNirrsyhsjL-Mrvh524",
+  authDomain: "hole-videos.firebaseapp.com",
+  projectId: "hole-videos",
+  storageBucket: "hole-videos.appspot.com",
+  messagingSenderId: "48275183097",
+  appId: "1:48275183097:web:aba2c4d81cd0ea592f5928",
+  measurementId: "G-VWP13CZLRL"
 };
 
 // Initialize Firebase
