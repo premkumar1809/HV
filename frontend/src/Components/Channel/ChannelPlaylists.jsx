f5c1cd0af73a5c13b1303be7737703c295d9915c
premkumar1809 <143527746+premkumar1809@users.noreply.github.com>
2024-03-01 12:17:47 +0000

    Update ChannelPlaylists.jsx

--- frontend/src/Components/Channel/ChannelPlaylists.jsx
+++ frontend/src/Components/Channel/ChannelPlaylists.jsx
@@ -20,7 +20,7 @@ function generateRandomColors(count) {
 }
 
 function ChannelPlaylists(prop) {
-  const backendURL = "https://youtube-clone-mern-backend.vercel.app"
+  const backendURL = "http://localhost:3000"
   const [PlaylistData, setPlaylistData] = useState([]);
   const [email, setEmail] = useState();
   const [playlistColors, setPlaylistColors] = useState([]);
