61b9a5a730236369a36d4611abc4635876e66a7a
premkumar1809 <143527746+premkumar1809@users.noreply.github.com>
2024-03-01 18:05:37 +0000

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import React from '@vitejs/plugin-react';
+// vite.config.js
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [React()],
-  build: {
-    rollupOptions: {
-      external: ['/src/main.jsx'],
-    },
-  },
-});
+export default {
+  plugins: [react()],
+};
