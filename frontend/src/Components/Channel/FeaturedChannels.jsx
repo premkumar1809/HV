8d6693379c663f21963fca314fa75826cd350d16
premkumar1809 <143527746+premkumar1809@users.noreply.github.com>
2024-03-01 17:24:06 +0000

    Update FeaturedChannels.jsx

--- frontend/src/Components/Channel/FeaturedChannels.jsx
+++ frontend/src/Components/Channel/FeaturedChannels.jsx
@@ -10,7 +10,7 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function FeaturedChannels(prop) {
-  const backendURL = "http://localhost:3000"
+  const backendURL = "https://hv-95uq.onrender.com"
   const [addChannelClicked, setAddChannelClicked] = useState(false);
   const [Subscriptions, setSubscriptions] = useState([]);
   const [Email, setEmail] = useState();
