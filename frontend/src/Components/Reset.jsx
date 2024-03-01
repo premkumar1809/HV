71bacb8471c8aafbeefd1f4b11076f0aecd2016c
premkumar1809 <143527746+premkumar1809@users.noreply.github.com>
2024-03-01 17:40:05 +0000

    Update Reset.jsx

--- frontend/src/Components/Reset.jsx
+++ frontend/src/Components/Reset.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function Reset() {
-  const backendURL = "http://localhost:3000"
+  const backendURL = "https://hv-95uq.onrender.com"
   const [email, setEmail] = useState("");
   const [BtnLoading, setBtnLoading] = useState(false);
   const [theme, setTheme] = useState(() => {
