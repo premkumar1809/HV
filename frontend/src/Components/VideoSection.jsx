995d00fbee7f6ff861072f98494dc2c938b4d2a2
premkumar1809 <143527746+premkumar1809@users.noreply.github.com>
2024-03-02 13:08:03 +0000

    Update VideoSection.jsx

--- frontend/src/Components/VideoSection.jsx
+++ frontend/src/Components/VideoSection.jsx
@@ -1069,16 +1069,7 @@ function VideoSection() {
             <video
               className="play-video"
               controls
-              "data-plyr-config='{
-  
-  "ads": {
-    "enabled": true,
-    "publisherId": "3623574",
-    "adTagUrl": "https://www.videosprofitnetwork.com/watch.xml?key=ff70a984693296dafd6c8ec3361b0765",
-    "adDuration": 15
-  }
-}'"
-
+              data-plyr-config='{  "ads": { "enabled": true, "publisherId": "22572239", "adTagUrl": "https://www.videosprofitnetwork.com/watch.xml?key=ff70a984693296dafd6c8ec3361b0765/ads?iu=/21775744923/external/single_preroll_skippable&sz=640x480&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=" } }'
               ref={videoRef}
               poster={thumbnailURL}
              
